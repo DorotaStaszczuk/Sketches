480d12a548c141c6d793d5a0397a9446bf0ecfb0
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2022-02-01 22:56:32 +0100

    starting point params added

--- sketch-04a.js
+++ sketch-04a.js
@@ -4,6 +4,18 @@ const settings = {
   dimensions: [ 2048, 2048 ]
 };
 
+const params = {
+    cols: 15,
+    rows: 15,
+    scaleMin: 1,
+    scaleMax: 30,
+    freq: 0.001,
+    amp: 0.2,
+    frame: 0,
+    animate: true,
+    lineCap: 'butt',
+};
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
