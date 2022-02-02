a5dc8741910d0bda9150aa166982ebf2b117f913
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2022-02-02 22:40:53 +0100

    grid added with noise to generate randomness, shadow added, parameters changed

--- sketch-04a.js
+++ sketch-04a.js
@@ -1,14 +1,17 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true,
 };
 
 const params = {
-    cols: 15,
-    rows: 15,
+    cols: 25,
+    rows: 25,
     scaleMin: 1,
-    scaleMax: 30,
+    scaleMax: 80,
     freq: 0.001,
     amp: 0.2,
     frame: 0,
@@ -17,10 +20,60 @@ const params = {
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-  };
+    return ({ context, width, height, frame }) => {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, width, height);
+
+        const cols = params.cols;
+        const rows = params.rows;
+        const numCells = cols * rows;
+
+        const gridw = width * 0.8;
+        const gridh = height * 0.8;
+
+        const cellw = gridw / cols;
+        const cellh = gridw / rows;
+
+        const margx = (width - gridw) * 0.5;
+        const margy = (height - gridh) * 0.5;
+
+        for (let i = 0; i < numCells; i++){
+            const col = i % cols;
+            const row = Math.floor(i / cols);
+
+            const x = col * cellw;
+            const y = row * cellh;
+
+            const w = cellw * 0.8;
+            const h = cellh * 0.8;
+
+            const f = params.animate ? frame : params.frame;
+            const n = random.noise3D(x, y, f * 10, params.freq);
+
+            const angle = n * Math.PI * params.amp;
+            const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
+
+            context.save();
+            context.translate(x, y);
+            context.translate(margx, margy);
+            context.translate(cellw * 0.5, cellh * 0.5);
+            context.rotate(angle);
+            context.strokeStyle = 'white';
+            context.lineWidth = scale;
+            context.lineCap = params.lineCap;
+            context.shadowColor = 'black';
+            context.shadowBlur = 8;
+            context.shadowOffsetX = 1;
+            context.shadowOffsetY = 1;
+
+            context.beginPath();
+            context.moveTo(w * -0.5, 0);
+            context.lineTo(w * 0.5, 0);
+            context.stroke();
+
+            context.restore();
+        }
+    };
 };
 
 canvasSketch(sketch, settings);
